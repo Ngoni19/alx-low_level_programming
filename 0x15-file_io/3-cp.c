591050188bcc7468e4db03edf824156ba2aac34d
Ngoni19 <ngoni19@live.com>
2022-08-22 21:49:43 +0000

    bug fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_r, fd_w, x, m, n;
+	int fd_rd, fd_wrt, k, j, n;
 	char buf[BUFSIZ];
 
 	if (argc != 3)
@@ -17,36 +17,37 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_r = open(argv[1], O_RDONLY);
-	if (fd_r < 0)
+	fd_rd = open(argv[1], O_RDONLY);
+	if (fd_rd < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((x = read(fd_r, buf, BUFSIZ)) > 0)
+	fd_wrt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((k = read(fd_rd, buf, BUFSIZ)) > 0)
 	{
-		if (fd_w < 0 || write(fd_w, buf, x) != x)
+		if (fd_wrt < 0 || write(fd_wrt, buf, k) != k)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_r);
+			close(fd_rd);
 			exit(99);
 		}
 	}
-	if (x < 0)
+	if (k < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	m = close(fd_r);
-	n = close(fd_w);
-	if (m < 0 || n < 0)
+	j = close(fd_rd);
+	n = close(fd_wrt);
+	if (j < 0 || n < 0)
 	{
-		if (m < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
+		if (j < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_rd);
 		if (n < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_wrt);
 		exit(100);
 	}
 	return (0);
-}}
+}
+
