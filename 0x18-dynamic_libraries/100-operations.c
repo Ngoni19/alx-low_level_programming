d4ddcab5e4aeb34ef6fe204a508fb67a3cabb977
Ngoni19 <ngoni19@live.com>
2022-10-03 22:03:46 +0000

    C dynamic library and python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+*add - The function adds two integers
+*@x: first integer
+*@y: second integer
+*Return: x + y
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
+/**
+ *sub - The function subtracts two integers
+ *@x: first integer
+ *@y: second integer
+ *Return: result
+ */
+int sub(int x, int y)
+{
+	return (x - y);
+}
+/**
+ *div - The function divides two integers
+ *@x: first integer
+ *@y: second integer
+ *Return: result
+ */
+int div(int x, int y)
+{
+	return (x / y);
+}
+/**
+ *mul - The function multiplies two integers
+ *@x: first integer
+ *@y: second integer
+ *Return: x * y
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+/**
+ *mod - The function finds mod of two integers
+ *@x: first integer
+ *@y: second integer
+ *Return: x % y
+ */
+int mod(int x, int y)
+{
+	return (x % y);
+}
